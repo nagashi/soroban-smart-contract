2b50c54ef310b48a10946f0ed47820e539b89c93
Charles ORiley <ceoriley@gmail.com>
2023-10-26 11:51:30 -0500

    Initial commit: hello-soroban & incrementor  contract

---
+++ contracts/incrementor/src/test.rs
@@ -0,0 +1,17 @@
+use super::{IncrementorContract, IncrementorContractClient};
+use soroban_sdk::{testutils::Logs, Env};
+
+extern crate std;
+
+#[test]
+fn increment() {
+    let env = Env::default();
+    let contract_id = env.register_contract(None, IncrementorContract);
+    let client = IncrementorContractClient::new(&env, &contract_id);
+
+    assert_eq!(client.increment(), 1);
+    assert_eq!(client.increment(), 2);
+    assert_eq!(client.increment(), 3);
+
+    std::println!("{}", env.logs().all().join("\n"));
+}
